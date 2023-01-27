5ca7695fabe5e99be23d2b78ac17ddf889df2e67
Leo9723 <pala9761@gmail.com>
2023-01-27 14:49:27 +0100

    first commit

---
+++ hello.php
@@ -0,0 +1,19 @@
+<?php
+    $paragraph = $_POST['paragraph'];
+    $hiddenWord = $_POST['hidden'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Paragrafo: <?php echo $paragraph ?></br> Lunghezza paragrafo: <?php echo strlen($paragraph) ?></h2>
+    <h2>Paragrafo: <?php echo str_replace($hiddenWord, '***', $paragraph) ?></br> Lunghezza paragrafo: <?php echo strlen($paragraph) ?></h2>
+</body>
+</html>
